6bd428b64edf078389e6e735d85290f9203481e7
dilane3 <komboudilane125@gmail.com>
2024-05-20 04:13:31 +0100

    feat: Loading image when on viewport enabled

--- src/components/image.tsx
+++ src/components/image.tsx
@@ -1,13 +1,88 @@
 import { useEffect, useState } from "react";
 import { ImageProps, LoadingFallbackProps } from "../types/index.js";
+import { uniqueId } from "../utils/index.js";
 
-export default ({ src, alt, style, ...props }: ImageProps) => {
+
+
+export default ({
+  src,
+  alt,
+  style,
+  loadingOnViewport = true,
+  ...props
+}: ImageProps & React.HTMLProps<HTMLImageElement>) => {
   // Local state
   const [loaded, setLoaded] = useState(false);
+  const [startLoading, setStartLoading] = useState(false);
   const [imageSrc, setImageSrc] = useState<HTMLImageElement>();
+  const [id] = useState(uniqueId()); // Generate unique ID for image element
 
   // Effects
+
+  /**
+   * Loads an image when it is in the viewport using the IntersectionObserver API.
+   * This effect is used to optimize image loading and reduce initial page load times.
+   *
+   * The effect first checks if the IntersectionObserver API is supported in the current browser.
+   * If not, it sets the `startLoading` state to `true` to load the image immediately.
+   *
+   * If the API is supported, the effect creates a new IntersectionObserver instance and observes
+   * the image element. When the image enters the viewport (i.e., the `isIntersecting` property
+   * of the observed entry is `true`), the `startLoading` state is set to `true` to trigger the
+   * image loading process. The observer is then disconnected to stop observing the element.
+   */
   useEffect(() => {
+    // Load image when it is in the viewport only
+
+    // Check if IntersectionObserver is supported
+    if (!("IntersectionObserver" in window)) {
+      setStartLoading(true);
+      return;
+    }
+
+    if (!loadingOnViewport) {
+      setStartLoading(true);
+
+      return;
+    }
+
+    // Create new IntersectionObserver
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        // If image is in the viewport, load it
+        if (entry.isIntersecting) {
+          setStartLoading(true);
+          observer.disconnect();
+        }
+      },
+      {
+        root: null,
+        rootMargin: "0px",
+        threshold: 0.1,
+      }
+    );
+
+    // Observe the image element
+    const imageElement = document.getElementById(id);
+
+    if (imageElement) {
+      observer.observe(imageElement);
+    }
+  }, []);
+
+  /**
+   * Preloads an image and updates the component's state when the image is loaded.
+   *
+   * This effect is triggered when the `src` or `startLoading` props change. It creates a new `Image` object, sets its `src` to the provided `src` prop, and listens for the `onload` event to update the component's state when the image is loaded.
+   *
+   * The effect also cleans up the `onload` event listener when the component is unmounted.
+   *
+   * @param src - The URL of the image to preload.
+   * @param startLoading - A boolean that triggers the preloading process when true.
+   */
+  useEffect(() => {
+    if (!startLoading) return;
+
     // Preload image
     const img = new Image();
 
@@ -16,10 +91,7 @@ export default ({ src, alt, style, ...props }: ImageProps) => {
       setLoaded(true);
     };
 
-    img.src =
-      src && (src as { uri: string }).uri
-        ? (src as { uri: string }).uri
-        : (src as string);
+    img.src = src as string;
 
     // Save image to state
     setImageSrc(img);
@@ -28,11 +100,12 @@ export default ({ src, alt, style, ...props }: ImageProps) => {
     return () => {
       img.onload = null;
     };
-  }, [src]);
+  }, [src, startLoading]);
 
   return (
     <>
       <div
+        id={id}
         style={{
           width: props.width || imageSrc?.width || 200,
           height: props.height || imageSrc?.height || 200,
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,7 +1,18 @@
 import React, { CSSProperties } from "react";
 
 /**
- * Props for the Image component
+ * Defines the props for an Image component.
+ *
+ * @property {string | { uri: string }} src - The source of the image, either a string URL or an object with a `uri` property.
+ * @property {string} alt - The alternative text for the image.
+ * @property {CSSProperties["width"]} [width] - The width of the image.
+ * @property {CSSProperties["height"]} [height] - The height of the image.
+ * @property {React.HTMLProps<HTMLDivElement>["className"]} [className] - The CSS class name to apply to the image container.
+ * @property {React.HTMLProps<HTMLDivElement>["style"]} [style] - The inline styles to apply to the image container.
+ * @property {"lazy" | "eager"} [loading] - The loading strategy for the image, either "lazy" or "eager".
+ * @property {"blur" | "wave"} [mode] - The loading mode for the image, either "blur" or "wave".
+ * @property {CSSProperties["objectFit"]} [objectfit] - The object-fit property for the image.
+ * @property {boolean} [loadingOnViewport] - Whether to load the image when it comes into the viewport.
  */
 export type ImageProps = {
   src: string | { uri: string };
@@ -13,10 +24,15 @@ export type ImageProps = {
   loading?: "lazy" | "eager";
   mode?: "blur" | "wave";
   objectfit?: CSSProperties["objectFit"];
-  onClick?: () => void;
+  loadingOnViewport?: boolean;
 };
 
+/**
+ * Defines the props for a loading fallback component.
+ * @property {CSSProperties["width"]} [width] - The width of the loading fallback.
+ * @property {CSSProperties["height"]} [height] - The height of the loading fallback.
+ */
 export type LoadingFallbackProps = {
   width?: CSSProperties["width"];
   height?: CSSProperties["height"];
-}
+};
